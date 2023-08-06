103095f53273d100783d8a1ee2679f6385a199a8
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-08-07 04:49:01 +0900

    🎨 Improve: format code

--- __main__.py
+++ __main__.py
@@ -7,26 +7,25 @@ from datetime import datetime
 print("Inference on", device.upper())
 
 torch_dtype = torch.float16 if device == "cuda" else torch.float32
-vae = AutoencoderKL.from_pretrained(vae, torch_dtype = torch_dtype).to(device)
+vae = AutoencoderKL.from_pretrained(vae, torch_dtype=torch_dtype).to(device)
 pipeline = StableDiffusionPipeline.from_pretrained(
-    model,
-    vae = vae,
-    torch_dtype = torch_dtype
+    model, vae=vae, torch_dtype=torch_dtype
 ).to(device)
 
 generator = torch.Generator(device=device).manual_seed(seed)
-if seed == -1: generator = generator.manual_seed(torch.seed())
+if seed == -1:
+    generator = generator.manual_seed(torch.seed())
 
 print("Seed:", generator.initial_seed())
 
 image = pipeline(
-    prompt = prompt,
-    negative_prompt = negative_prompt,
-    num_inference_steps = steps,
-    width = width,
-    height = height,
+    prompt=prompt,
+    negative_prompt=negative_prompt,
+    num_inference_steps=steps,
+    width=width,
+    height=height,
     guidance_scale=scale,
-    generator=generator
+    generator=generator,
 ).images[0]
 
 filename = f"outputs/{datetime.now().strftime('%Y%m%d%H%M%S%f')}.png"
--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ model = "gsdf/Counterfeit-V2.5"
 
 # ex:
 # "stabilityai/sd-vae-ft-mse", "stabilityai/sd-vae-ft-ema"
-# 
+#
 # You can specify it as follows if you prepared VAE locally:
 # "vae/counterfeit-v2.5"
 vae = "stabilityai/sd-vae-ft-mse"
