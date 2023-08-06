126036a170627f29f97f119bd5e37a4f0e5222c6
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-08-07 04:08:42 +0900

    ✨ Add: VAE support

--- __main__.py
+++ __main__.py
@@ -1,14 +1,17 @@
-from settings import infer_on, model
-from diffusers import StableDiffusionPipeline
+from settings import infer_on, vae, model
+from diffusers import StableDiffusionPipeline, AutoencoderKL
 import torch
 from config import prompt, negative_prompt, steps, width, height, scale
 from datetime import datetime
 
 print("Inference on", infer_on.upper())
 
+torch_dtype = torch.float16 if infer_on == "cuda" else torch.float32
+vae = AutoencoderKL.from_pretrained(vae, torch_dtype = torch_dtype).to(infer_on)
 pipeline = StableDiffusionPipeline.from_pretrained(
     model,
-    torch_dtype = torch.float16 if infer_on == "cuda" else torch.float32
+    vae = vae,
+    torch_dtype = torch_dtype
 ).to(infer_on)
 
 image = pipeline(
