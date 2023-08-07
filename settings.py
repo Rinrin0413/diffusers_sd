06528688094aba1064e16a8e258ae3831962def3
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-08-07 09:33:49 +0900

    🔥 Remove: unnecessary line

--- settings.py
+++ settings.py
@@ -5,9 +5,6 @@ repo = "gsdf/Counterfeit-V2.5"
 # ex:
 # "stabilityai/sd-vae-ft-mse", "stabilityai/sd-vae-ft-ema"
 #
-# You can specify it as follows if you prepared VAE locally:
-# "vae/counterfeit-v2.5"
-#
 # "auto" for automatic detection from repo
 vae = "auto"
 
